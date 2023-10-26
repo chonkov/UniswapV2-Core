9c5faa3b28e85d3e4a2f89ae25954c7890039f2c
grc02 <georgi.chonkov27@gmail.com>
2023-10-26 21:55:25 +0300

    Modified 'Factory' interface and contract

--- src/Factory.sol
+++ src/Factory.sol
@@ -1,50 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
+import {Ownable2Step, Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable2Step.sol";
 import {IFactory} from "./interfaces/IFactory.sol";
 import {Pair} from "./Pair.sol";
 
-// Remove those and make Factory 'Ownable2Step' ?
+error Factory_Identical_Addresses();
+error Factory_Zero_Address();
+error Factory_Pair_Already_Exists();
 
-contract Factory is IFactory {
-    address public feeTo;
-    address public feeToSetter;
+contract Factory is IFactory, Ownable2Step {
+    address private _feeTo;
 
-    mapping(address => mapping(address => address)) public getPair;
-    address[] public allPairs;
+    mapping(address => mapping(address => address)) private _pairs;
+    address[] private _allPairs;
 
-    constructor(address _feeToSetter) {
-        feeToSetter = _feeToSetter;
-    }
-
-    function allPairsLength() external view returns (uint256) {
-        return allPairs.length;
-    }
+    constructor(address _owner) Ownable(_owner) {}
 
     function createPair(address tokenA, address tokenB) external returns (address pair) {
-        require(tokenA != tokenB, "IDENTICAL_ADDRESSES");
-        require(tokenA != address(0), "ZERO_ADDRESS");
+        if (tokenA == tokenB) revert Factory_Identical_Addresses();
+        if (tokenA == address(0)) revert Factory_Zero_Address();
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(getPair[token0][token1] == address(0), "PAIR_EXISTS");
+        if (_pairs[token0][token1] != address(0)) revert Factory_Pair_Already_Exists();
         bytes memory bytecode = type(Pair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
         Pair(pair).initialize(token0, token1);
-        getPair[token0][token1] = pair;
-        getPair[token1][token0] = pair;
-        allPairs.push(pair);
-        emit PairCreated(token0, token1, pair, allPairs.length);
+        _pairs[token0][token1] = pair;
+        _pairs[token1][token0] = pair;
+        _allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, _allPairs.length);
     }
 
-    function setFeeTo(address _feeTo) external {
-        require(msg.sender == feeToSetter, "FORBIDDEN");
-        feeTo = _feeTo;
+    function setFeeTo(address feeTo_) external onlyOwner {
+        _feeTo = feeTo_;
     }
 
-    function setFeeToSetter(address _feeToSetter) external {
-        require(msg.sender == feeToSetter, "FORBIDDEN");
-        feeToSetter = _feeToSetter;
+    function feeTo() external view returns (address) {
+        return _feeTo;
+    }
+
+    function allPairsLength() external view returns (uint256) {
+        return _allPairs.length;
+    }
+
+    function getPair(address tokenA, address tokenB) external view returns (address) {
+        return _pairs[tokenA][tokenB];
+    }
+
+    function allPairs(uint256 _index) external view returns (address) {
+        return _allPairs[_index];
     }
 }
--- src/interfaces/IFactory.sol
+++ src/interfaces/IFactory.sol
@@ -5,7 +5,6 @@ interface IFactory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint256);
 
     function feeTo() external view returns (address);
-    function feeToSetter() external view returns (address);
 
     function getPair(address tokenA, address tokenB) external view returns (address pair);
     function allPairs(uint256) external view returns (address pair);
@@ -14,5 +13,4 @@ interface IFactory {
     function createPair(address tokenA, address tokenB) external returns (address pair);
 
     function setFeeTo(address) external;
-    function setFeeToSetter(address) external;
 }
