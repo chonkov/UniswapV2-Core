1ab6100a39c59348ec987094ec8ab5332134e446
grc02 <georgi.chonkov27@gmail.com>
2023-10-27 20:10:55 +0300

    Implemented 'swap' functionality of the 'Pair' contract

--- src/Pair.sol
+++ src/Pair.sol
@@ -1,8 +1,106 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
+import {Context} from "lib/openzeppelin-contracts/contracts/utils/Context.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IPair} from "./interfaces/IPair.sol";
+import {ICallee} from "./interfaces/ICallee.sol";
+import {ShareToken} from "./ShareToken.sol";
 
-contract Pair is IPair {
-    function initialize(address token0, address token1) external {}
+error Pair_Locked();
+// error Pair_Access_Denied();
+error Pair_Insufficient_Amounts();
+error Pair_Insufficient_Liquidity();
+error Pair_Invalid_Receiver();
+
+contract Pair is IPair, Context, ShareToken {
+    using SafeERC20 for IERC20;
+
+    uint256 public constant MINIMUM_LIQUIDITY = 10 ** 3;
+
+    address public immutable factory;
+    address public immutable token0;
+    address public immutable token1;
+
+    uint112 private reserve0;
+    uint112 private reserve1;
+    uint32 private blockTimestampLast;
+
+    uint256 public price0CumulativeLast;
+    uint256 public price1CumulativeLast;
+    uint256 public kLast; // reserve0 * reserve1, as of immediately after the most recent liquidity event
+
+    bool private unlocked = true;
+
+    modifier lock() {
+        if (!unlocked) revert Pair_Locked();
+        unlocked = false;
+        _;
+        unlocked = true;
+    }
+
+    constructor(address _token0, address _token1) {
+        factory = _msgSender();
+        token0 = _token0;
+        token1 = _token1;
+    }
+
+    // update reserves and, on the first call per block, price accumulators
+    function _update(uint256 balance0, uint256 balance1, uint112 _reserve0, uint112 _reserve1) private {}
+
+    // this low-level function should be called from a contract which performs important safety checks
+    function swap(uint256 amount0Out, uint256 amount1Out, address to, bytes calldata data) external lock {
+        if (amount0Out == 0 && amount1Out == 0) revert Pair_Insufficient_Amounts();
+        (uint112 _reserve0, uint112 _reserve1,) = getReserves();
+
+        if (amount0Out >= _reserve0 || amount1Out >= _reserve1) revert Pair_Insufficient_Liquidity();
+
+        uint256 balance0;
+        uint256 balance1;
+        {
+            // scope for _token{0,1}, avoids stack too deep errors
+
+            address _token0 = token0;
+            address _token1 = token1;
+            if (to == _token0 || to == _token1) revert Pair_Invalid_Receiver();
+
+            if (amount0Out > 0) IERC20(_token0).safeTransfer(to, amount0Out);
+            if (amount1Out > 0) IERC20(_token1).safeTransfer(to, amount1Out);
+            if (data.length > 0) ICallee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
+            balance0 = IERC20(_token0).balanceOf(address(this));
+            balance1 = IERC20(_token1).balanceOf(address(this));
+        }
+
+        uint256 amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
+        uint256 amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
+        require(amount0In > 0 || amount1In > 0, "UniswapV2: INSUFFICIENT_INPUT_AMOUNT");
+        {
+            // scope for reserve{0,1}Adjusted, avoids stack too deep errors
+            uint256 balance0Adjusted = balance0 * 1000 - amount0In * 3;
+            uint256 balance1Adjusted = balance1 * 1000 - amount1In * 3;
+            require(balance0Adjusted * balance1Adjusted >= uint256(_reserve0) * _reserve1 * 1000 ** 2, "UniswapV2: K");
+        }
+
+        _update(balance0, balance1, _reserve0, _reserve1);
+        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
+    }
+
+    function skim(address to) external lock {
+        // Cache storage variables
+        address _token0 = token0;
+        address _token1 = token1;
+        IERC20(_token0).safeTransfer(to, IERC20(_token0).balanceOf(address(this)) - reserve0);
+        IERC20(_token1).safeTransfer(to, IERC20(_token1).balanceOf(address(this)) - reserve1);
+    }
+
+    function sync() external {
+        _update(IERC20(token0).balanceOf(address(this)), IERC20(token1).balanceOf(address(this)), reserve0, reserve1);
+    }
+
+    function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) {
+        _reserve0 = reserve0;
+        _reserve1 = reserve1;
+        _blockTimestampLast = blockTimestampLast;
+    }
 }
